ca94c5991a15c170312f1b6c8fe3b2d48c8a1914
Brian Mo <Brian.Mo@ibm.com>
2019-09-04 15:42:45 -0400

    fix: Deploy site resolves to correct group.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,14 +24,14 @@ const siteConfig = {
   title: 'Sprout Creek Farm Retail Site', // Title for your website.
   tagline: 'Project Resources Documentation',
   url: 'https://pages.github.com', // Your website URL
-  baseUrl: '/marist-project/', // Base URL for your project */
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'docs',
-  organizationName: 'dTT',
+  organizationName: 'yum-yum-eat-em-up',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
